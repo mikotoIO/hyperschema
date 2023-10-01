eb30a9ef31be4406db5055c60b3e38b4ec49a755
TheCactusBlue <thecactusblue@gmail.com>
2023-10-02 01:16:20 +1030

    fix the bug with the broken typing

--- packages/typescript/hyperschema-core/src/hrpc/event.ts
+++ packages/typescript/hyperschema-core/src/hrpc/event.ts
@@ -3,9 +3,9 @@ import { ZodType, z } from 'zod';
 type CleanupFn = (() => void) | void;
 
 export class HyperRPCEvent<Ctx, T extends ZodType> {
-  emitterSetup?: (emit: (x: z.infer<ZodType>) => void, ctx: Ctx) => CleanupFn;
+  emitterSetup?: (emit: (x: z.infer<T>) => void, ctx: Ctx) => CleanupFn;
   constructor(public eventType: T) {}
-  emitter(setup: (emit: (x: z.infer<ZodType>) => void, ctx: Ctx) => CleanupFn) {
+  emitter(setup: (emit: (x: z.infer<T>) => void, ctx: Ctx) => CleanupFn) {
     this.emitterSetup = setup;
     return this;
   }
