91aa893e63a08f85ef32e953fb96301c45f92412
TheCactusBlue <thecactusblue@gmail.com>
2023-10-01 16:29:50 +1030

    improve type of hsdate

--- packages/typescript/hyperschema-core/src/types.ts
+++ packages/typescript/hyperschema-core/src/types.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export function hsDate(): z.ZodEffects<z.ZodString, string, Date> {
+export function hsDate(): z.ZodEffects<z.ZodString, string, Date | string> {
   return z.preprocess(
     (val) => (val instanceof Date ? val.toISOString() : val),
     z.string(),
