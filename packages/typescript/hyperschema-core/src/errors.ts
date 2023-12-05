5f597585eac3e02ac17c6d6083442ddafa1057cd
TheCactusBlue <thecactusblue@gmail.com>
2023-12-06 04:38:34 +1000

    more error types

--- packages/typescript/hyperschema-core/src/errors.ts
+++ packages/typescript/hyperschema-core/src/errors.ts
@@ -13,9 +13,21 @@ export class NotFoundError extends BaseError {
   }
 }
 
+export class AlreadyExistsError extends BaseError {
+  constructor(message?: string) {
+    super('AlreadyExists', message);
+  }
+}
+
 export class UnauthorizedError extends BaseError {
   constructor(message?: string) {
     super('Unauthorized', message);
+  }
+}
+
+export class PermissionDeniedError extends BaseError {
+  constructor(message?: string) {
+    super('PermissionDenied', message);
   }
 }
 
@@ -25,8 +37,32 @@ export class ValidationError extends BaseError {
   }
 }
 
+export class BadRequestError extends BaseError {
+  constructor(message?: string) {
+    super('BadRequest', message);
+  }
+}
+
 export class ServerError extends BaseError {
   constructor(message?: string) {
     super('ServerError', message);
+  }
+}
+
+export class TimeoutError extends BaseError {
+  constructor(message?: string) {
+    super('Timeout', message);
+  }
+}
+
+export class TooManyRequestsError extends BaseError {
+  constructor(message?: string) {
+    super('TooManyRequests', message);
+  }
+}
+
+export class UnimplementedError extends BaseError {
+  constructor(message?: string) {
+    super('Unimplemented', message);
   }
 }
